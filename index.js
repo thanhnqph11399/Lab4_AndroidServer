1cadc737b394ad0cebb2c15c5cb154198c721391
Quang Thanh <59902482+Thanhnq1608@users.noreply.github.com>
2021-04-05 00:20:16 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var express =require('express');
+var app=express();
+
+var expressHbs=require('express-handlebars');
+
+app.use('/assets',express.static(__dirname+'/assets'));
+//thiết lập thư viện handlebars
+app.engine('handlebars',expressHbs({
+  //thiết lập thư mục và tên của file layout cha
+  layoutsDir: __dirname+'/views/layouts',
+  defaultLayout: 'base'
+}))
+app.set('view engine','handlebars')
+
+
+
+//lawngs nghe  http get
+app.get('/',function (req, res) {
+  res.render('index');
+});
+app.get('/home',function (req, res) {
+  res.render('home');
+});
+app.get('/account',function (req, res) {
+  res.render('account');
+});
+app.get('/login',function (req, res) {
+  res.render('login');
+});
+//lắng nghe http post
+app.get('/', function (req, res) {
+
+});
+
+app.listen(process.env.PORT || '3002')
+
